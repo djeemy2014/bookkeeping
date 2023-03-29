e4a7cc9532ca8c809a06e64fa298435f573e7552
Dmitiy Demidyuk work <djeemy2014@gmail.com>
2023-03-29 17:52:24 +0300

    Readme.me

--- scripts/readExelFile.js
+++ scripts/readExelFile.js
@@ -7,7 +7,7 @@ import cp1251 from './decodingToUTF8.js'
 const input_file = "C:\\Users\\Администратор\\Downloads\\operations Wed Mar 01 09_16_50 MSK 2023-Mon Mar 06 19_46_21 MSK 2023.csv";
 const output_file = "C:\\Users\\Администратор\\Downloads\\ALL20230313.xlsx";
 const output_csv="C:\\Users\\Администратор\\Downloads\\operations Wed Mar 01 09_16_50 MSK 2023-Mon Mar 06 19_46_21 MSK 2023_UTF8.csv";
-const regexpUTF8=/[А-я]/g
+const regexpUTF8=/[А-я]|Ё|ё/g
 
 
 //function decodingFile(input_file){
@@ -21,7 +21,7 @@ if (regexpUTF8.test(file_utf_class.input)){
     console.log('ОК')
     const file_utf=file_utf_class.input
 }else{
-    console.log('БЛииииин')
+    console.log('БЛииииин Нужна перекодировка')
     const file_utf=file_utf_class.decoding()
 };
 
